478c3f1176fe47950059fb63c506d3f1d4fd3e1b
Hossein Naderi <hossein-naderi@hotmail.com>
2023-05-17 11:56:00 +0330

    Supports gzip, deflate and has firefox headers

--- modules/scraper/src/main/scala/Fetcher.scala
+++ modules/scraper/src/main/scala/Fetcher.scala
@@ -5,12 +5,14 @@ import cats.effect.std.Semaphore
 import cats.effect.syntax.all.*
 import io.odin.Logger
 import org.http4s.Header
+import org.http4s.MediaType
 import org.http4s.Method.GET
 import org.http4s.Request
 import org.http4s.Uri
 import org.http4s.client.Client
 import org.http4s.client.dsl.io.*
 import org.http4s.client.middleware.FollowRedirect
+import org.http4s.client.middleware.GZip
 
 import java.net.URI
 import java.util.concurrent.TimeoutException
@@ -26,44 +28,61 @@ object Fetcher {
     juri =>
       buildUri(juri)
         .flatMap { uri =>
-          val req = GET(
+          val req = GET.apply(
             uri,
             Header(
               "User-Agent",
-              "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0"
-            )
+              "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/112.0"
+            ),
+            Header(
+              "Accept",
+              "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
+            ),
+            Header("Accept-Language", "en-US,en;q=0.5"),
+            Header("DNT", "1"),
+            Header("Upgrade-Insecure-Requests", "1"),
+            Header("Connection", "keep-alive"),
+            Header("Sec-Fetch-Dest", "document"),
+            Header("Sec-Fetch-Mode", "navigate"),
+            Header("Sec-Fetch-Site", "none"),
+            Header("Sec-Fetch-User", "?1")
           )
 
-          client
-            .run(req)
-            .use { resp =>
-              val baseURI = FollowRedirect
-                .getRedirectUris(resp)
-                .lastOption
-                .map(_.toString)
-                .map(URI.create)
-                .getOrElse(juri)
+          logger.debug(s"request: ${req.asCurl(_ => false)}") >>
+            client
+              .run(req)
+              .use { resp =>
+                val redirected = FollowRedirect
+                  .getRedirectUris(resp)
+                val baseURI = redirected.lastOption
+                  .map(_.toString)
+                  .map(URI.create)
+                  .getOrElse(juri)
 
-              resp.bodyText.compile.string.timed.map((time, body) =>
-                FetchedData(
-                  url = baseURI,
-                  resp.headers.headers.map(h => (h.name.toString, h.value)),
-                  resp.status.code,
-                  HttpVersion(resp.httpVersion.major, resp.httpVersion.minor),
-                  resp.cookies
-                    .map(rc =>
-                      Cookie(
-                        name = rc.name,
-                        content = rc.content,
-                        domain = rc.domain,
-                        path = rc.path
-                      )
+                logger.debug(s"redirected $redirected") >>
+                  resp.bodyText.compile.string.timed.map((time, body) =>
+                    FetchedData(
+                      url = baseURI,
+                      resp.headers.headers.map(h => (h.name.toString, h.value)),
+                      resp.status.code,
+                      HttpVersion(
+                        resp.httpVersion.major,
+                        resp.httpVersion.minor
+                      ),
+                      resp.cookies
+                        .map(rc =>
+                          Cookie(
+                            name = rc.name,
+                            content = rc.content,
+                            domain = rc.domain,
+                            path = rc.path
+                          )
+                        )
+                        .toSet,
+                      body
                     )
-                    .toSet,
-                  body
-                )
-              )
-            }
+                  )
+              }
         }
         .timeout(timeoutDuration)
         .map(Right(_))
@@ -94,9 +113,9 @@ object Fetcher {
   def apply(
       client: Client[IO],
       timeout: FiniteDuration = 5.seconds,
-      maxRedirects: Int = 3
+      maxRedirects: Int = 5
   )(using Logger[IO]): IO[Fetcher] = simple(
-    FollowRedirect(maxRedirects)(client),
+    FollowRedirect(maxRedirects)(GZip()(client)),
     timeout
   )
 
