3cb5c38e91118a45958918ba2467003bd83ca308
Hossein Naderi <hossein-naderi@hotmail.com>
2023-07-14 15:47:00 +0330

    Colorized banner

--- modules/common/src/main/scala/CMDApp.scala
+++ modules/common/src/main/scala/CMDApp.scala
@@ -10,7 +10,7 @@ abstract class CMDApp[T](cmd: Command[T]) extends IOApp {
   protected given logger: Logger[IO]
 
   override final def run(args: List[String]): IO[ExitCode] =
-    IO.println(CMDApp.banner).as(cmd.parse(args, sys.env)).flatMap {
+    IO.println(CMDApp.bannerColored).as(cmd.parse(args, sys.env)).flatMap {
       case Left(value) => IO.println(value).as(ExitCode.Success)
       case Right(value) =>
         for {
@@ -31,8 +31,20 @@ abstract class CMDApp[T](cmd: Command[T]) extends IOApp {
 }
 
 object CMDApp {
-  private val banner = s"""
-${Console.YELLOW}
+
+  extension (inline banner: String) {
+    inline def colorize(inline char: String, inline colors: String): String = {
+      val startPattern = s"([^$char])$char".r
+      val endPattern = s"$char([^$char])".r
+
+      endPattern.replaceAllIn(
+        startPattern.replaceAllIn(banner, s"$$1$colors$char"),
+        s"$char${Console.RESET}$$1"
+      )
+    }
+  }
+
+  private inline val banner = """
 
                 ,, ,,                     ,,,,,,,,,               ,
              ,,       ,,        ,,,*(((((((((,,,,,,.           ,,  ,,
@@ -40,19 +52,24 @@ ${Console.YELLOW}
        ,,               ,,,,*(((((((((((((((((((((((((,,,,            ,,
    ,,,                  ,,,(((((((*,((((((((((((/,/(((((,,              ,
                ,,,    ,,,*((((,*@@@@@@@,((((,,@@@@@@@,,(((,,,            ,,
-              ,     ,,(*/((((,@@@@@,,&&&,*(,&@@@@#,&&&,,(((,,             .,
-            ,,      ,  ,(((((,@@@@@,&&&&,*(,@@@@@,,&&&#,(((,,,    ,,,       ,,
+              ,     ,,(*/((((,@@@@@,,&&&,*(,,@@@@,,&&&,,(((,,             .,
+            ,,      ,  ,(((((,@@@@@,&&&&,*(,@@@@@,,&&&,,(((,,,    ,,,       ,,
           ,,          .,,,*((/,@@@@@@@@,,(((,(@@@@@@@(,((((,,,       ,       .,
          ,,             ,,,(((((/,,,,,(((((((((,,,,,(((((,,,,,        ,
        ,,           ,,,,,  ,(/((((((((((((,((((((((((((((,,            ,
       ,            ,,      ,,,*(((((((((((((((((((((((,,,,,,            ,
-                  ,,          ,(,,,(,((((((((((*(,,,,,       ,           ,
+                  ,,           ,,,,(,((((((((((*(,,,,,       ,           ,
                  ,,            ,             ,,,             ,            ,
                  ,                                            ,
                 ,                                             ,
                ,                                              .,
                                                                ,
 
-${Console.RESET}
 """
+  private val bannerColored =
+    banner
+      .colorize("\\(", Console.YELLOW + Console.YELLOW_B)
+      .colorize("@", Console.WHITE_B)
+      .colorize("&", Console.CYAN_B + Console.CYAN)
+      .colorize(",", Console.MAGENTA)
 }
